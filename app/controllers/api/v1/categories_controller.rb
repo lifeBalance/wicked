6a3d4de6b5579015b16610806d06c025608395cc
Javier Rodriguez <javierorodriguez@gmail.com>
2015-11-20 19:18:27 +0300

    Change controller name

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,10 +1,10 @@
-class Api::V1::CategoryController < ApplicationController
+class Api::V1::CategoriesController < ApplicationController
   respond_to :json
 
   def show
     category = params[:category] ? params[:category] : "sports"
     @category = Category.where(:cat_title => category.capitalize).first
 
-    render :json => @category, serializer: CategorySerializer, root: "category"
+    render :json => @category, serializer: CategorySerializer, :root => 'category'
   end
 end
